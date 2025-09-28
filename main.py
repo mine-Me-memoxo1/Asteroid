b804446294b09c4a3072df8b9a27d7f10eba2b16
labhi10.20 <labhi10.20@proton.me>
2025-09-27 22:35:35 -0400

    Added clock functionality to limit to 60 fps to not consume too much of computer resources

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ def main():
     print("Screen width: {}\nScreen height: {}".format(SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while 1:
         screen.fill('black')
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 if __name__ == "__main__":
     main()
