f11d3f83bf7af518126dd90e6086ed83608bfc2e
labhi10.20 <labhi10.20@proton.me>
2025-09-28 12:10:56 -0400

    Added PLAYER_RADIOUS to constants.py and created player.py containing the Player class. I also created a .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 def main():
     print("Starting Asteroids!")
     print("Screen width: {}\nScreen height: {}".format(SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -7,8 +8,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while 1:
         screen.fill('black')
+        player.draw(screen)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
