bc3257bed7f7c03373fe03340f970eae9d0cd4b5
labhi10.20 <labhi10.20@proton.me>
2025-09-30 22:37:54 -0400

    Added Shooting functionality and rate limiting to the shooting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import *
 from asteroid import *
 from asteroidfield import *
 import sys
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -15,10 +16,12 @@ def main():
     
     updatable, drawable = pygame.sprite.Group(), pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     ast_field = AsteroidField()
--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 import circleshape
 from constants import *
 import pygame
+from shot import Shot
+
 class Player(circleshape.CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -32,7 +35,17 @@ class Player(circleshape.CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt)
+        if keys[pygame.K_SPACE]:
+            if self.timer <= 0:
+                self.shoot(dt)
+        self.timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self, dt):
+        self.timer = PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        
